3e9af87c592c739e464b98b4e3b7c9792d1a62be
meifatang <me@tangmeifa.com>
2023-03-14 16:37:23 +0800

    modify

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Meifa Tang\'s blog.';
+export const SITE_TITLE = 'Meifa\'s blog';
 export const SITE_DESCRIPTION = 'A minimalism blog with less style. Just foucs on wrting.';
---
+++ src/content/blog/design-a-minimalism-chatbot.md
@@ -0,0 +1,9 @@
+---
+title: "Design a minimalism Chatbot powered by GPT-3.5.-Turbo"
+description: ""
+pubDate: "March 14 2022"
+---
+
+https://chat.tangmeifa.com/
+
+OpenAI `/v1/chatcompletion` api
